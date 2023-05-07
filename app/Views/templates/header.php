41c5f588ffd6653f38fddbd4ca8d486d94742afb
crhisgbibon <admin@crhisgbibon.com>
2023-05-07 20:06:47 +0100

    update

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,9 +1,15 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
-    <title>Wordle+</title>
-    <link rel="stylesheet" type="text/css" href="output.css" />
-    <link rel="stylesheet" type="text/css" href="main.css" />
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js" crossorigin="anonymous"></script>
+  <meta charset="UTF-8" />
+  <title>Wordle+</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="description" content="" />
+  <link href="https://cdn.jsdelivr.net/npm/modern-normalize@v1.1.0/modern-normalize.min.css" rel="stylesheet">
+  <link rel="icon" href="favicon.ico">
+  <link rel="manifest" href="manifest.webmanifest">
+  <link rel="stylesheet" type="text/css" href="output.css" />
+  <link rel="stylesheet" type="text/css" href="main.css" />
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js" crossorigin="anonymous"></script>
 </head>
 <body>
